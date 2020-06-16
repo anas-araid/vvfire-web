2b6f8ba30f956d0abb952268d79b87c2432ad567
asdf1899 <araid.anas99@gmail.com>
2020-06-16 17:07:27 +0200

    Fix homepage

--- src/App.vue
+++ src/App.vue
@@ -10,5 +10,6 @@
     background-color:#fafafa!important;
     text-align: center;
     color: #2c3e50;
+    height: 100%!important;
   }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,9 @@
         <md-icon class="style-red-text">menu</md-icon>
       </md-button>
       <span class="md-title style-red-text"><b>VVF</b>ire</span>
+      <div class="md-toolbar-section-end">
+        <md-button to="/login" class="style-red-text">Accedi</md-button>
+      </div>
     </md-app-toolbar>
 
     <md-app-drawer :md-active.sync="showNavigation" md-swipeable md-fixed style="background-color:white">
@@ -38,10 +41,10 @@
     <md-app-content style="height:100%">
       <div class="md-layout md-alignment-center-center" style="background:#fafafa; min-height:500px;height:100%">
         <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100" style="text-align:left">
-          <span class="md-display-2" style="font-weight:200">Changing the way we <i>rescue</i>...</span>
+          <span class="md-display-2" style="font-weight:200">Changing the way we <i class="style-red-text">rescue</i></span>
         </div>
         <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
-          <span class="md-display-3">Display 3</span>
+          <span class="md-display-3">[screen app]</span>
         </div>
       </div>
     </md-app-content>
