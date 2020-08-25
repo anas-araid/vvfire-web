73b45d216d03f2df70672e50aea233e27992aef7
asdf1899 <araid.anas99@gmail.com>
2020-08-25 23:19:22 +0200

    Fix ricerche view

--- src/views/RicercaPersona.vue
+++ src/views/RicercaPersona.vue
@@ -2,7 +2,47 @@
   <md-app-content style="height:100%;border:none">
       <div class="md-layout md-alignment-center-center">
         <Dialog v-if="this.message.active" :data="this.message"></Dialog>
+        <md-card style="overflow-x:auto" v-if="!this.loading">
+        <md-card-content>
+          <div v-if="this.allRicerche.length !== 0 && !this.errored" style="overflow-x:auto">
+            <md-table>
+              <md-table-toolbar>
+                <h1 class="md-title">Lista ricerche</h1>
+              </md-table-toolbar>
+              <md-table-row>
+                <md-table-head class="style-table-header">NOME RICERCA</md-table-head>
+                <md-table-head class="style-table-header">INIZIO RICERCA</md-table-head>
+                <md-table-head class="style-table-header">FINE RICERCA</md-table-head>
+                <md-table-head class="style-table-header">STATO</md-table-head>
+                <md-table-head class="style-table-header"></md-table-head>
+                <md-table-head class="style-table-header"></md-table-head>
+              </md-table-row>
+              <md-table-row v-for="ricerca in this.allVigili" :key="ricerca.id">
+                <md-table-head md-label="name">{{ricerca.name}}</md-table-head>
+                <md-table-head md-label="startTime">{{ricerca.startTime}}</md-table-head>
+                <md-table-head md-label="endTime">{{ricerca.endTime}}</md-table-head>
+                <md-table-head md-label="completed">{{ricerca.completed ? 'SI' : 'NO'}}</md-table-head>
+                <md-table-head md-label="modifica"><a @click="openModificaRicerca(ricerca.id)">MODIFICA</a></md-table-head>
+                <md-table-head md-label="elimina"><a class="style-red-text" @click="alertDeleteRicerca(vigile.id)">RIMUOVI</a></md-table-head>
+              </md-table-row>
+            </md-table>
+          </div>
+          <div v-else-if="this.errored" style="text-align:center">
+            <h3>Errore</h3>
+          </div>
+          <div v-else-if="!this.datiPresenti" style="text-align:center">
+            <h3>Dati non presenti</h3>
+          </div>
+          </md-card-content>
+        </md-card>
+        <div v-else>
+          <md-progress-spinner class="md-accent" md-mode="indeterminate"></md-progress-spinner>
+        </div>
       </div>
+    <md-button class="md-fab md-fab-bottom-right" @click="temp()" :disabled="this.loading">
+      <md-icon>add</md-icon>
+    </md-button>
+    <br>
   </md-app-content>
 </template>
 
@@ -15,8 +55,9 @@ export default {
     showNavigation: false,
     loading: false,
     message: {'active': false, 'content': null, 'url': null},
-    events: [],
-    selectedEvent: null
+    datiPresenti: false,
+    allRicerche: [],
+    errored: false
   }),
   components: {
     'Dialog': DialogAlert,
