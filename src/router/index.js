fb7c3840c9549b15a1ba3f07b59c451b8a594cb7
asdf1899 <araid.anas99@gmail.com>
2020-09-19 13:47:00 +0200

    Add landing page

--- src/router/index.js
+++ src/router/index.js
@@ -29,6 +29,7 @@ const routes = [
       }
     },{
       path: 'login',
+      name: 'Login',
       components: {
         homepage: Login
       }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,49 @@
 <template>
-  <md-app-content style="height:100%;border:none">
-    <div class="md-layout md-alignment-center-center" style="min-height:500px;height:100%">
-      <div class="md-layout-item md-large-size-50 md-medium-size-33 md-small-size-50 md-xsmall-size-100" style="text-align:left">
-        <span class="md-display-2" style="font-weight:200">Changing the way we <i class="style-red-text">rescue</i></span>
+  <md-app-content :style="'border:0;height:'+ (fullHeight - 140) +'px'">
+    <div class="md-layout md-alignment-center-center" style="min-height:100%;height:100%">
+      <div class="md-layout-item md-large-size-70 md-medium-size-70 md-small-size-50 md-xsmall-size-100" style="text-align:left">
+        <span class="md-display-3" style="font-weight:200">Changing the way we <i class="style-red-text">rescue</i></span><br>
+        <span class="md-display-1" style="font-weight:200">L'innovazione a portata di mano</span>
         <br><br><br>
-        <button class="circleScaleBtn"><span>INIZIA SUBITO!</span></button>
+        <button class="circleScaleBtn" @click="router.push({name: 'Login'})"><span>INIZIA SUBITO!</span></button>
         <button class="snapLeftBtn"><span>SCOPRI DI PIU'</span></button>
       </div>
-      <div class="md-layout-item md-large-size-50 md-medium-size-33 md-small-size-50 md-xsmall-size-100">
-        <span class="md-display-3">[screen app]</span>
+      <div class="md-layout-item md-large-size-30 md-medium-size-30 md-small-size-50 md-xsmall-size-100">
+        <img src="../assets/homepage.svg"/>
       </div>
+    </div>
+    <div>
+      <md-icon class="arrowBounce">keyboard_arrow_down</md-icon>
     </div>
   </md-app-content>
 </template>
 
 <script>
   // @ is an alias to /src
-  
+  import router from '../router/index.js';
+
   export default {
     name: 'Home',
     data: () => ({
-      showNavigation: false
-    })
+      showNavigation: false,
+      fullHeight:document.documentElement.clientHeight,
+      router: router
+    }),
+    create(){
+      window.addEventListener("resize", this.myEventHandler());
+    },
+    mounted() {
+      window.addEventListener('resize', this.handleResize)
+    },
+    beforeDestroy: function () {
+      window.removeEventListener('resize', this.handleResize)
+    },
+    methods: {
+      handleResize (event) {
+        console.log(event)
+        this.fullHeight = document.documentElement.clientHeight
+      }
+    }
   }
 </script>
 <style lang="scss" scoped>
